ead44c9fc4085b059268bf924a67c6d557b12a5d
david <35779726+DavidPrecopia@users.noreply.github.com>
2018-08-20 18:28:26 -0400

    Display a joke via a Toast when the button is pressed in the Fragment.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.jokelibrary.Jokes;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.databinding.FragmentMainBinding;
@@ -19,6 +21,8 @@ public class MainActivityFragment extends Fragment {
 
     private FragmentMainBinding binding;
 
+    private Toast toast;
+
     public MainActivityFragment() {
     }
 
@@ -27,6 +31,30 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
 
+        setUpOnClickListener();
+        setUpAdView();
+
+        return binding.getRoot();
+    }
+
+    private void setUpOnClickListener() {
+        binding.buttonTellJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jokeToast();
+            }
+        });
+    }
+
+    private void jokeToast() {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(getContext(), Jokes.getJoke(), Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private void setUpAdView() {
         AdView mAdView = binding.getRoot().findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
@@ -35,7 +63,5 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
-
-        return binding.getRoot();
     }
 }
